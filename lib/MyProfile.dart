bd80a803256ced6d58ec41eb0c3ad65095cd54db
Samik Maharjan <samikmaharjan55@gmail.com>
2023-04-13 14:07:29 +0545

    Updated

--- lib/MyProfile.dart
+++ lib/MyProfile.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:form_app/Home_Screen.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:form_app/Login_Screen.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 
 class MyProfile extends StatefulWidget {
   const MyProfile({Key? key}) : super(key: key);
@@ -10,6 +16,29 @@ class MyProfile extends StatefulWidget {
 }
 
 class _MyProfileState extends State<MyProfile> {
+  File? image;
+  Future pickImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) return;
+
+      // final imageTemporary = File(image.path);
+      //setState(() => this.image = imageTemporary);
+      final imagePermanent = await saveImagePermanently(image.path);
+      setState(() => this.image = imagePermanent);
+    } on PlatformException catch (e) {
+      debugPrint('Failed to pick image: $e');
+    }
+  }
+
+  Future<File> saveImagePermanently(String imagePath) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final name = basename(imagePath);
+    final image = File('${directory.path}/$name');
+
+    return File(imagePath).copy(image.path);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,27 +53,174 @@ class _MyProfileState extends State<MyProfile> {
             padding: const EdgeInsets.all(8.0),
             child: Row(
               children: [
-                Stack(
-                  children: [
-                    Container(
-                      height: 90,
-                      width: 90,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        borderRadius: BorderRadius.circular(50),
+                image != null
+                    ? GestureDetector(
+                        onTap: () {
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return Dialog(
+                                  child: SizedBox(
+                                    height: 80,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        SizedBox(
+                                          width: double.infinity,
+                                          child: GestureDetector(
+                                            onTap: () {
+                                              Navigator.pop(context);
+                                              pickImage(ImageSource.gallery);
+                                            },
+                                            child: Card(
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                children: const [
+                                                  Icon(FontAwesome.photo),
+                                                  SizedBox(
+                                                    width: 20.0,
+                                                  ),
+                                                  Text(
+                                                    'Gallery',
+                                                    style: TextStyle(
+                                                      fontSize: 20.0,
+                                                      color: Colors.black,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: double.infinity,
+                                          child: GestureDetector(
+                                            onTap: () {
+                                              Navigator.pop(context);
+                                              pickImage(ImageSource.camera);
+                                            },
+                                            child: Card(
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                children: const [
+                                                  Icon(Entypo.camera),
+                                                  SizedBox(
+                                                    width: 20.0,
+                                                  ),
+                                                  Text(
+                                                    'Camera',
+                                                    style: TextStyle(
+                                                      fontSize: 20.0,
+                                                      color: Colors.black,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              });
+                        },
+                        child: ClipOval(
+                          child: Image.file(
+                            image!,
+                            width: 80,
+                            height: 80,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      )
+                    : GestureDetector(
+                        onTap: () {
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return Dialog(
+                                  child: SizedBox(
+                                    height: 80,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        SizedBox(
+                                          width: double.infinity,
+                                          child: GestureDetector(
+                                            onTap: () {
+                                              Navigator.pop(context);
+                                              pickImage(ImageSource.gallery);
+                                            },
+                                            child: Card(
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                children: const [
+                                                  Icon(FontAwesome.photo),
+                                                  SizedBox(
+                                                    width: 20.0,
+                                                  ),
+                                                  Text(
+                                                    'Gallery',
+                                                    style: TextStyle(
+                                                      fontSize: 20.0,
+                                                      color: Colors.black,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: double.infinity,
+                                          child: GestureDetector(
+                                            onTap: () {
+                                              Navigator.pop(context);
+                                              pickImage(ImageSource.camera);
+                                            },
+                                            child: Card(
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                children: const [
+                                                  Icon(Entypo.camera),
+                                                  SizedBox(
+                                                    width: 20.0,
+                                                  ),
+                                                  Text(
+                                                    'Camera',
+                                                    style: TextStyle(
+                                                      fontSize: 20.0,
+                                                      color: Colors.black,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              });
+                        },
+                        child: Image.asset(
+                          'images/profilephoto.png',
+                          height: 80,
+                          width: 80,
+                        ),
                       ),
-                      child: Icon(Icons.person),
-                    ),
-                    Positioned(
-                      top: 0,
-                      left: 60,
-                      child: Icon(
-                        Icons.add,
-                        size: 25,
-                      ),
-                    ),
-                  ],
-                ),
                 SizedBox(
                   width: 10,
                 ),
